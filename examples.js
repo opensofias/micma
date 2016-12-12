ea853ba0e5ddae7e75aba9f12365383ba1299dd9
opensofias <sofias@radikalismus.com>
2016-12-12 19:01:13 +0100

    removed the rest of the non-functional examples

--- examples.js
+++ examples.js
@@ -1,18 +1,1 @@
-function gf2test ()
-{
-	var gf2 = GF2 ()
-	var preterm = [1,1,"+",1, 1,"*"]
-	var term = new Term (preterm,gf2)
-	return term.evaluate()
-}
-
-function underflowTest ()  //does strange things, not sure if i care
-{
-	var gf2 = GF2 ()
-	var preterm = [1,"+"]
-	var term = new Term (preterm,gf2)
-	return term.evaluate ()
-}
-
-
 // todo: new test-routine
